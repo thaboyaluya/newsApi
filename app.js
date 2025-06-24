3bf6c2b93b123db278066a622561c247d6393511
Zan Aluya <rumealuya3@gmail.com>
2025-06-24 18:13:12 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 const limiter=rateLimit({
-    max:300,
+    max:50,
     windowMs:60*60*1000,
     message:'Too many request from this IP,please try again in an hour!'
 })
